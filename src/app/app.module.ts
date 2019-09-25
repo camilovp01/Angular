eb7af0d4b2c78c1e704fffd170f79c12a87a32f7
Camilo Velasquez <camilo.velasquezp@gmail.com>
2019-09-24 22:11:08 -0500

    Primer commit angular avanzado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { DasboardComponent } from './pages/dasboard/dasboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './login/login.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    DasboardComponent,
+    ProgressComponent,
+    LoginComponent,
+    Graficas1Component,
+    NopagefoundComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
